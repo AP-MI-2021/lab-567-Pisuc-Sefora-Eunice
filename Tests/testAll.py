f1da3325156ae18c2c829e51ed81cd2e815a5f09
Pisuc-Sefora-Eunice <sefora.pisuc@stud.ubbcluj.ro>
2021-11-01 20:12:34 +0200

    Lab5 terminat

--- Tests/testAll.py
+++ Tests/testAll.py
@@ -1,9 +1,10 @@
-from Tests.testCRUD import testAdaugaObiect, testModificaObiect, testGetById
+from Tests.testCRUD import testAdaugaObiect, testModificaObiect, testGetById, testStergeObiect
 from Tests.testDomeniu import testObiect
 
 
 def runAllTests():
     testObiect()
     testAdaugaObiect()
+    testStergeObiect()
     testModificaObiect
     testGetById
