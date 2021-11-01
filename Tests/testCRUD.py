007fec3fff279cfe73a46f0e4d98fc87056f2321
Pisuc-Sefora-Eunice <sefora.pisuc@stud.ubbcluj.ro>
2021-11-01 20:04:11 +0200

    Lab5 terminat

--- Tests/testCRUD.py
+++ Tests/testCRUD.py
@@ -35,35 +35,19 @@ def testModificaObiect():
     lista = adaugaObiect("1", "caiet", "A4", 10, "mall", lista)
     lista = adaugaObiect("2", "pix", "negru", 3, "dm", lista)
     lista = modificaObiect("1", "penar", "mic", 15, "compas", lista)
-    updated_object = getById("1", lista)
-    assert getId(updated_object) == "1"
-    assert getNume(updated_object) == "penar"
-    assert getDescriere(updated_object) == "mic"
-    assert getPret(updated_object) == 15
-    assert getLocatie(updated_object) == "compas"
-    out_dated_object = getById("2", lista)
-    assert getId(out_dated_object) == "2"
-    assert getNume(out_dated_object) == "pix"
-    assert getDescriere(out_dated_object) == "negru"
-    assert getPret(out_dated_object) == 3
-    assert getLocatie(out_dated_object) == "dm"
-    lista = []
-    lista = adaugaObiect("1", "caiet", "A4", 10, "mall", lista)
-    lista = modificaObiect("3", "penar", "mic", 15, "compas", lista)
-    out_dated_object = getById("1", lista)
-    assert getId(out_dated_object) == "1"
-    assert getNume(out_dated_object) == "caiet"
-    assert getDescriere(out_dated_object) == "A4"
-    assert getPret(out_dated_object) == 10
-    assert getLocatie(out_dated_object) == "mall"
+    modificareObiect = getById("1", lista)
+    assert getId(modificareObiect) == "1"
+    assert getNume(modificareObiect) == "penar"
+    assert getDescriere(modificareObiect) == "mic"
+    assert getPret(modificareObiect) == 15
+    assert getLocatie(modificareObiect) == "compas"
+
 
 
 def testGetById():
     lista = []
     lista = adaugaObiect("1", "caiet", "A4", 10, "mall", lista)
     lista = adaugaObiect("2", "pix", "negru", 3, "dm", lista)
-    assert getId(getById(2, lista)) == 2
-    assert getNume(getById(2, lista)) == "pix"
-    assert getDescriere(getById(2, lista)) == "negru"
-    assert getPret(getById(2, lista)) == 3
-    assert getLocatie(getById(2, lista)) == "dm"
+
+    assert getById("2", lista) is not None
+    assert getById("3", lista) is None
